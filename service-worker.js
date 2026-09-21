996d769de6597608486e1576d618c3d3ad4aad3b
xcodehunt-collab <xcodehunt@gmail.com>
2026-09-21 09:59:54 +0530

    Refresh cache for regional news release

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='global-news-hub-v8-image-fix';
+const CACHE_NAME='global-news-hub-v9-regional';
 const STATIC_ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./logo.svg'];
 
 self.addEventListener('install',event=>{
